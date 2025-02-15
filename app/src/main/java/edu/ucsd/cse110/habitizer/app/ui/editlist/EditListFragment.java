5d23fda4d8b82f6237b6ccfbcc1e1af9dc5ae0b3
Ferretelic <kyleshouki@gmail.com>
2025-02-15 06:11:49 -0800

    removed unused import

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/editlist/EditListFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/editlist/EditListFragment.java
@@ -5,8 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.text.InputType;
 import android.widget.EditText;
 
 
